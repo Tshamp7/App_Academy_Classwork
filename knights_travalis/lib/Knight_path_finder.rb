7381d18cba6366a0f59ea95375a8cfec7218259b
Tom <T.shamp7@gmail.com>
2020-04-02 16:32:22 -0700

    knight path finder build_move_tree complete

--- knights_travalis/lib/Knight_path_finder.rb
+++ knights_travalis/lib/Knight_path_finder.rb
@@ -1,13 +1,20 @@
 require_relative "00_tree_node.rb"
+require "byebug"
 
 class KnightPathFinder
-    attr_reader :root_node
+    attr_accessor :start_pos, :considered_positions, :root_node
     def initialize(pos)
-        @root_node = PolyTreeNode.new(pos)
-        @move_tree = self.build_move_tree
+        @start_pos = pos
+        @considered_positions = [pos]
+        @root_node = PolyTreeNode.new(@start_pos)
+
+
+
+        build_move_tree
     end
 
-    MOVES = [
+    MOVES = 
+    [
         [-2, -1],
         [-2,  1],
         [-1, -2],
@@ -16,12 +23,58 @@ class KnightPathFinder
         [ 1,  2],
         [ 2, -1],
         [ 2,  1]
-      ]
-    def self.new_move_positions
+    ]
 
-    end    
+    def valid_moves(pos) #build list of all valid moves.... moves are valid if they fall in valid coordinates on a 8x8 standard chess board using indexes of 0-7 X and 0-7 Y.
+        valid_moves = []
 
-    def valid_moves(pos)
-        @considered positions = [pos]
+        x = pos[0]
+        y = pos[1]
+
+        MOVES.each do |move|
+            new_move = [x + move[0], y + move[1]]
+
+            valid_moves << new_move if new_move.all? { |coord| coord.between?(0, 7) }
+        end
+
+        valid_moves
     end
+
+    def new_move_positions(pos)
+        moves = valid_moves(pos)
+        moves.reject! { |move| @considered_positions.include?(move) }
+        moves.each do |move|
+            @considered_positions << move
+        end
+        moves
+    end
+
+    def build_move_tree
+        nodes = [@root_node]
+
+        until nodes.empty?
+            node = nodes.shift
+            current_pos = node.value
+            next_moves = new_move_positions(current_pos)
+            next_moves.each do |next_move|
+                child = PolyTreeNode.new(next_move)
+                nodes << child
+                node.add_child(child)
+            end
+        end
+        
+    end
+
+
+
+
+
+
+
 end
+
+
+
+arr = [0, 0]
+knight = KnightPathFinder.new(arr)
+p knight
