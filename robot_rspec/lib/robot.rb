e93d89805260946e2d356791c747256bd8a6759c
Tom <T.shamp7@gmail.com>
2020-04-16 11:45:41 -0700

    Complete robot rspec, add all weapon methods

--- robot_rspec/lib/robot.rb
+++ robot_rspec/lib/robot.rb
@@ -1,8 +1,11 @@
 class Robot
-  attr_reader :items, :position
-  attr_writer :items, :position
+  attr_accessor :items, :position, :items_weight, :health, :equipped_weapon
   def initialize
-      @position = [0, 0]
+    @position = [0, 0]
+    @items = []
+    @items_weight = 0
+    @health = 100
+    @equipped_weapon = nil
   end
 
   def move_left
@@ -29,21 +32,99 @@ class Robot
     @position = [position[0] += vertical, position[1] += horizontal]
   end
 
-  def items
-    @items = []
+  class HeavyItemError < ArgumentError
+    def message
+        puts "Thats too heavy for me to pickup!"
+    end
   end
+
 
   def pick_up(item)
-    self.items << item
+      if items_weight + item.weight <= 250
+        @items << item
+      else
+        raise ArgumentError
+      end
   end
 
+  def items_weight
+    if @items.empty?
+      return @items_weight = 0
+    else
+      return @items_weight = @items.map(&:weight).sum
+    end
+  end
+
+  def wound(amount)
+    if (health - amount).negative? 
+      @health = 0
+    else
+      @health = health - amount
+    end
+  end
+
+  def heal(amount)
+    if (health + amount) > 100 
+      @health = 100
+    else
+      @health = health + amount
+    end
+  end
+
+  def attack(robot)
+    if equipped_weapon.nil?
+      robot.wound(5)
+    else
+      equipped_weapon.hit(robot)
+    end
+  end
 end
 
 class Item
   attr_accessor :name, :weight
-  def initialize(name, weight)
+  def initialize(name, weight = 0)
     @name = name
     @weight = weight
   end
+end
 
+class Bolts < Item
+  attr_accessor :bolts, :name, :weight
+  def initialize
+    @name = 'bolts'
+    @weight = 25
+  end
+
+  def feed(robot)
+    robot.heal(weight)
+  end
+end
+
+class Weapon < Item
+    attr_accessor :name, :weight, :damage
+    def initialize(name, weight, damage)
+        @name = name
+        @weight = weight
+        @damage = damage
+    end
+
+    def hit(robot)
+        robot.wound(damage)
+    end
+end
+
+class Laser < Weapon
+  def initialize
+    @name = 'laser'
+    @weight = 125
+    @damage = 25
+  end
+end
+
+class PlasmaCannon < Weapon
+  def initialize
+    @name = 'plasma cannon'
+    @weight = 200
+    @damage = 55
+  end
 end
--- robot_rspec/spec/robot_spec.rb
+++ robot_rspec/spec/robot_spec.rb
@@ -188,7 +188,7 @@ describe "PlasmaCannon" do
 
   it "initializes as a Weapon with the correct name, weight, and damage" do
     expect(plasma_cannon).to be_a(Weapon)
-    expect(plasma_cannon.name).to eq("plasma_cannon")
+    expect(plasma_cannon.name).to eq("plasma cannon")
     expect(plasma_cannon.weight).to eq(200)
     expect(plasma_cannon.damage).to eq(55)
   end
