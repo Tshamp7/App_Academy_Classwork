796ae1775c0271a5033bdea8202330295e5d95b0
Tom <T.shamp7@gmail.com>
2020-05-04 09:04:48 -0700

    my_hash_map finished

--- my_hash_map/lib/p01_int_set.rb
+++ my_hash_map/lib/p01_int_set.rb
@@ -5,94 +5,104 @@ class MaxIntSet
     @store = Array.new(max, false)
   end
 
-  def insert(num)
-    validate!(num)
-    @store[num] = true unless include?(num)
-  end
-
-  def remove(num)
-    @store[num] = false if include?(num)
-  end
-
   def include?(num)
-    if @store[num] == true
+    return true if self.store[num] == true
+    return false
+  end
+
+  def insert(num)
+    raise "Out of bounds" unless num.between?(0, @max)
+    if self.store[num] == false && num.between?(0, @max)
+      self.store[num] = true
       true
     else
       false
     end
   end
 
-  private
-
-  def is_valid?(num)
-    num.between?(0, @max)
+  def remove(num)
+    if self.include?(num)
+      self.store[num] = false
+    else
+      p "set does not contain #{num}"
+    end
   end
-
-  def validate!(num)
-    raise 'Out of bounds' unless is_valid?(num)
-  end
+ 
 end
 
 
 class IntSet
+  attr_reader :store
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
-  def insert(num)
-    bucket = num % 20
-    @store[bucket] << num unless include?(num)
-    true
-  end
+  def include?(num)
+    bucket = num % num_buckets
 
-  def remove(num)
-    bucket = num % 20
-    if @store[bucket].include?(num)
-      @store[bucket].delete(num)
+    if self.store[bucket].include?(num)
+      true
+    else
+      false
     end
   end
 
-  def include?(num)
-    bucket = num % 20
-    @store[bucket].include?(num)
+  def insert(num)
+    bucket = num % num_buckets
+
+    self.store[bucket] << num unless self.include?(num)
   end
+
+  def remove(num)
+    bucket = num % num_buckets
+    if self.include?(num)
+      store[bucket].delete(num)
+      true
+    else
+      false
+    end
+  end
+
+
+  private
+
+  def num_buckets
+    self.store.length
+  end
+  
 
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_reader :count, :store
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
+  def include?(num)
+    self[num].include?(num)
+  end
+
   def insert(num)
-    bucket = num % num_buckets
-     unless include?(num)
-      @store[bucket] << num 
-      @count += 1
-     end
-     resize! if num_buckets < @count
+    return false if include?(num)
+    self[num] << num
+    @count += 1
+    resize! if @count >= num_buckets
   end
 
   def remove(num)
-    bucket = num % num_buckets
     if include?(num)
-      @store[bucket].delete(num)
+      self[num].delete(num)
       @count -= 1
     end
-  end
-
-  def include?(num)
-    bucket = num % num_buckets
-    @store[bucket].include?(num)
   end
 
   private
 
   def [](num)
     bucket = num % num_buckets
-    @store[bucket]
+    self.store[bucket]
   end
 
   def num_buckets
@@ -101,8 +111,8 @@ class ResizingIntSet
 
   def resize!
     old_store = @store
-    @store = Array.new(num_buckets * 2) { Array.new }
     @count = 0
-    old_store.flatten.map { |num| insert(num) }
+    @store = Array.new(num_buckets * 2) { Array.new }
+    old_store.flatten.each { |num| insert(num) }
   end
 end
